de55bc8565fe24644e0cbed5ff3948c9549d8ed1
Sasa Buklijas <buklijas.sasa@gmail.com>
2024-08-20 15:59:33 +0200

    init

---
+++ 1/app.py
@@ -0,0 +1,25 @@
+import os
+import random
+
+# list of quotes
+awesome_quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "In the middle of every difficulty lies opportunity. - Albert Einstein",
+    "The best way to predict the future is to create it. - Peter Drucker",
+    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "Success is not in what you have, but who you are. - Bo Bennett",
+    "The harder you work for something, the greater you'll feel when you achieve it. - Unknown",
+    "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it. - Patrick McKenzie",
+]
+
+
+def main():
+    quote = random.choice(awesome_quotes)
+    print(quote)
+
+
+if __name__ == "__main__":
+    main()
